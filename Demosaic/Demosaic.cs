a3634039569a7e8fe4b2dc75d0c11b4b0b2ac723
AUTOMATIC1111 <16777216c@gmail.com>
2018-04-29 14:22:25 +0300

    Demosaic mod

---
+++ Demosaic/Demosaic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BepInEx;
+using Harmony;
+
+namespace Demosaic
+{
+    public static class Hooks
+    {
+        public static void InstallHooks()
+        {
+            try
+            {
+                var harmony = HarmonyInstance.Create("info.jbcs.koikatsu.demosaic");
+                harmony.PatchAll(typeof(Hooks));
+            }
+            catch (Exception e)
+            {
+                BepInLogger.Log(e.ToString(), false);
+            }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaControl), "LateUpdateForce")]
+        public static void LateUpdateForce(ChaControl __instance)
+        {
+            __instance.hideMoz = true;
+        }
+
+    }
+
+    [BepInPlugin(GUID: "info.jbcs.koikatsu.demosaic", Name: "Demosaic", Version: "1.0.0")]
+    public class Demosaic : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Hooks.InstallHooks();
+        }
+    }
+}
